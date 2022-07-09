a09770df1071a9bf950e7157500816b2474c3752
Duska <Duska@DESKTOP-O3AR66J>
2022-07-09 19:43:14 +0200

    Add project files.

---
+++ CalculateAngle/BusinessLogic/CalculateLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalculateAngle.BusinessLogic
+{
+    public class CalculateLogic
+    {
+        public static int CalculateAngle(double hour, double minute)
+        {
+            if (hour > 12)
+                hour -= 12;
+            
+            int hoursInDegrees = (int)(0.5 * (hour * 60 + minute));
+            int minutesInDegrees = (int)(6 * minute);
+            int angle = Math.Abs(hoursInDegrees - minutesInDegrees);
+            return angle = Math.Min(360 - angle, angle);
+        }
+    }
+}
---
+++ CalculateAngle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CalculateAngle.BusinessLogic;
+
+namespace CalculateAngle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateLogic.CalculateAngle(21, 0));
+        }
+    }
+}
